13de86c924255ed0e1428fef308d1ddc0924478d
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-24 12:45:40 -0600

    Completed Better curve code example & printed mean

--- better_curved_scores.py
+++ better_curved_scores.py
@@ -1,4 +1,18 @@
 # Software Engineering Practices Pt 1 tutorial from Machine Learning Engineering Nanodegree - Udacity -> Input by Ryan L Buchanan 24OCT20
 
 import math
-import 
+import numpy as np
+
+def flat_curve(arr, n):
+    return [i + n for i in arr]
+
+def square_root_curve(arr):
+    return [math.sqrt(i) * 10 for i in arr]
+
+test_scores = [88, 92, 79, 93, 85]
+curved_5 = flat_curve(test_scores, 5)
+curved_10 = flat_curve(test_scores, 10)
+curved_sqrt = square_root_curve(test_scores)
+
+for score_list in test_scores, curved_5, curved_10, curved_sqrt:
+    print(np.mean(score_list))
